18807c48508cc36913a4aca76ab916e18afa9180
shalaroiqqq <shalaroi.0719@gmail.com>
2026-03-10 19:15:51 +0700

    Add files via upload

---
+++ Lab8_68046200/myproject/web/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+def form(request):
+    return render(request, 'form.html')
+
+def about(request):
+    return render(request, 'about.html')
